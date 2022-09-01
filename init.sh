e82797f2eee15ce2aa09eea60b98c612371bf0b2
Dzianis Ravinski <delgoden@gmail.com>
2022-09-01 15:47:50 +0300

    edit init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
   sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
   sudo rm -rf /etc/nginx/sites-enabled/default
-  sudo nginx -s reload
+  #sudo nginx -s reload
   gunicorn -c etc/hello.py hello:app
-  #sudo /etc/init.d/nginx restart
+  sudo /etc/init.d/nginx restart
