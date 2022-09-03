6e74868bf4f21ca0ca30bb1de9265e18c0dbec16
Dzianis Ravinski <delgoden@gmail.com>
2022-09-03 12:01:25 +0300

    new edits

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
   sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
   sudo rm -rf /etc/nginx/sites-enabled/default
   #sudo nginx -s reload
-  gunicorn -c etc/hello.py hello:app
   sudo /etc/init.d/nginx restart
+  exec gunicorn --bind="0.0.0.0:8080" hello:app
