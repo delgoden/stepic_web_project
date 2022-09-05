54605763d66dc5630d4835e984caf75e9dc654d0
Dzianis Ravinski <delgoden@gmail.com>
2022-09-05 18:46:29 +0300

    new homework

--- init.sh
+++ init.sh
@@ -1,5 +1,6 @@
   sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
   sudo rm -rf /etc/nginx/sites-enabled/default
-  #sudo nginx -s reload
-  sudo /etc/init.d/nginx restart
-  exec gunicorn --bind=0.0.0.0:8080 hello:app
+  sudo nginx -s reload
+  #sudo /etc/init.d/nginx restart
+  cd ask
+  exec gunicorn --bind=0.0.0.0:8000 ask.wsgi
